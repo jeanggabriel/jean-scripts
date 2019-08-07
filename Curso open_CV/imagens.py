fe3fac6abb6eba0f7cbe1f7ad6585ef9e805e7ce
jean gabriel <jeangabriel310@gmail.com>
2019-08-07 16:15:13 -0300

    desenvolvimento

--- Curso open_CV/imagens.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-imagemCarregada = cv2.imread("../sonic.jpg",0)
-cv2.imshow("imagemCarregada",imagemCarregada)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-cv2.imwrite("imagemSalva.png",imagemCarregada)
---
+++ curso em video/array.py
@@ -0,0 +1,10 @@
+while True:        
+    a = str(input('nome:'))
+    b = str(input('telefone: '))
+    c = str(input('cpf: '))
+
+    d= [a ,b ,c]
+
+    for i in (a,b,c,d):
+        print(i)
+        break
